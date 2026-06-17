2b1959c48d7cefc4297c1d72b79805be19d3a1e4
Harsha <harsha@example.com>
2026-06-17 15:39:57 +0530

    Update app.py

--- Household_Energy_Consumption_Forecaster/app.py
+++ Household_Energy_Consumption_Forecaster/app.py
@@ -48,19 +48,72 @@ import csv
 
 from flask import make_response
 
+# ==========================================
+# DATABASE INITIALIZATION
+# ==========================================
+
 def init_db():
 
     connection = sqlite3.connect("database.db")
 
     cursor = connection.cursor()
 
+    # USERS TABLE
+
     cursor.execute(
         """
-        CREATE TABLE IF NOT EXISTS users (
+        CREATE TABLE IF NOT EXISTS users(
+
             id INTEGER PRIMARY KEY AUTOINCREMENT,
+
             username TEXT NOT NULL,
+
             email TEXT UNIQUE NOT NULL,
-            password TEXT NOT NULL
+
+            password TEXT NOT NULL,
+
+            profile_picture TEXT DEFAULT 'default.png',
+
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+        )
+        """
+    )
+
+    # PREDICTIONS TABLE
+
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS predictions(
+
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+            user_id INTEGER,
+
+            global_reactive_power REAL,
+
+            voltage REAL,
+
+            global_intensity REAL,
+
+            sub_metering_1 REAL,
+
+            sub_metering_2 REAL,
+
+            sub_metering_3 REAL,
+
+            predicted_power REAL,
+
+            category TEXT,
+
+            advice TEXT,
+
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+            FOREIGN KEY(user_id)
+
+            REFERENCES users(id)
+
         )
         """
     )
@@ -69,6 +122,9 @@ def init_db():
 
     connection.close()
 
+    print(
+        "Database initialized successfully!"
+    )
 # ==========================================
 # CREATE APP
 # ==========================================
