9118aaf0eb6c9ec91579a0d0da2177e86af34193
Harsha <harsha@example.com>
2026-06-17 13:54:15 +0530

    Update app.py

--- Household_Energy_Consumption_Forecaster/app.py
+++ Household_Energy_Consumption_Forecaster/app.py
@@ -80,8 +80,9 @@ app.config["UPLOAD_FOLDER"] = "static/uploads"
 app.secret_key = os.getenv("SECRET_KEY")
 
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_TIMEOUT"] = 10
 app.config["MAIL_USERNAME"] = os.getenv(
     "MAIL_USERNAME"
